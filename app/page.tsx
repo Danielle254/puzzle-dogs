db0ae7f078d2b545dc6b8c0c47928e0403565c82
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-09-06 09:23:08 -0500

    feat: add page section components to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
+import Hero from "./components/Hero";
+import Intro from "./components/Intro";
+import StayConnected from "./components/StayConnected";
+import UpcomingLitters from "./components/UpcomingLitters";
+
 export default function Home() {
-  return <></>;
+  return (
+    <div>
+      <Hero />
+      <Intro />
+      <UpcomingLitters />
+      <StayConnected />
+    </div>
+  );
 }
