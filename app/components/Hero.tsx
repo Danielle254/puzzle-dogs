6c6a3b26f9a856e357a6816800c91e84d7b63a86
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-09-06 09:23:16 -0500

    feat: create hero section for home page

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,3 +1,11 @@
 export default function Hero() {
-  return <div></div>;
+  return (
+    <section>
+      <img
+        src="/images/pan_hero_2.jpg"
+        alt="small brown and white dog runs across a grass field"
+        className="w-full h-[calc(100vh-80px)]"
+      />
+    </section>
+  );
 }
