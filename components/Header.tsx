6c950e0e91b2a33e526cf32c5be842444cac01a1
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-09-06 08:58:46 -0500

    feat: create header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,50 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+
 export default function Header() {
-  return <div></div>;
+  return (
+    <nav className="flex justify-between max-w-[1000px] h-20 mx-auto items-center">
+      <div className="flex items-end">
+        <img
+          src="/placeholder_logo.jpg"
+          className="max-h-16"
+          alt="Puzzle Dogs Logo - lime green dog with purple cape leaps up and to the right"
+        />{" "}
+        {/* need to replace with PNG logo file */}
+        <h1 className="text-xl font-bold italic">Puzzle Dogs</h1>
+      </div>
+      <ul className="flex gap-4 items-center">
+        <li>
+          <Button
+            variant="link"
+            asChild
+            className="font-bold text-lg text-foreground "
+          >
+            <Link href="/">Home</Link>
+          </Button>
+        </li>
+        <li>
+          <Button
+            variant="link"
+            asChild
+            className="font-bold text-lg text-foreground"
+          >
+            <Link href="/about">About</Link>
+          </Button>
+        </li>
+        <li>
+          <Button
+            variant="link"
+            asChild
+            className="font-bold text-lg text-foreground"
+          >
+            <Link href="/ourdogs">Our Dogs</Link>
+          </Button>
+        </li>
+        <li>
+          <Button>Learn More</Button>
+        </li>
+      </ul>
+    </nav>
+  );
 }
