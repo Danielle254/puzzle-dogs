9dca38fc51a972e4ae0a4fc5a9afbde2ab313d47
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-09-07 08:16:13 -0500

    feat: create section

--- app/components/UpcomingLitters.tsx
+++ app/components/UpcomingLitters.tsx
@@ -1,3 +1,37 @@
+import { Button } from "@/components/ui/button";
+
 export default function UpcomingLitters() {
-  return <div></div>;
+  return (
+    <section className="py-12 px-4 bg-primary/10">
+      <div className="max-w-[1000px] mx-auto ">
+        <h2>Upcoming Litters</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
+          <article>
+            <img
+              src="/images/puppy_running.jpg"
+              className="shadow rounded-sm w-full h-60 object-cover object-center"
+            />
+            <h3 className="font-bold text-xl py-4">Biz x Stud Fall 2025</h3>
+            <p className="text-lg pb-8">
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+              commodo ligula eget
+            </p>
+            <Button>Facebook Group</Button>
+          </article>
+          <article>
+            <img
+              src="/images/biz_profile.jpg"
+              className="shadow rounded-sm w-full h-60 object-cover object-center"
+            />
+            <h3 className="font-bold text-xl py-4">Biz x Stud Summer 2026</h3>
+            <p className="text-lg pb-8">
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+              commodo ligula eget
+            </p>
+            <Button>Learn More</Button>
+          </article>
+        </div>
+      </div>
+    </section>
+  );
 }
