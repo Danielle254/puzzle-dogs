cc6a7794b11cabd2b7a49c950dc7eaf46093a103
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-09-13 16:13:18 -0500

    style: remove background color from section

--- app/about/components/BreedingPhilosophy.tsx
+++ app/about/components/BreedingPhilosophy.tsx
@@ -3,34 +3,33 @@ import Link from "next/link";
 
 export default function BreedingPhilosophy() {
   return (
-    <section className="bg-primary/10 ">
-      <div className="max-w-[1000px] mx-auto py-12 px-4">
-        <h2>Breeding Philosophy</h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 pb-8">
-          <p className="text-lg col-span-2">
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-            commodo ligula eget dolor. Aenean massa. Cum sociis natoque
-            penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-            Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
-            <br />
-            <br />
-            Nulla consequat massa quis enim. Donec pede justo, fringilla vel,
-            aliquet nec, vulputate eget, arcu. In eni pretium. Integer
-            tincidunt. Cras dapibus. Vivam
-            <br />
-            <br />
-            Nulla consequat massa quis enim. Donec pede justo, fringilla vel,
-            aliquet nec, vulputate eget, arcu. In eni
-          </p>
-          <img
-            src="/images/iroh.jpg"
-            className="rounded-sm shadow col-span-1"
-          />
-        </div>
-        <Button variant="outline" asChild>
-          <Link href="https://www.facebook.com/puzdog/">LEARN MORE</Link>
-        </Button>
+    <section className="max-w-[1000px] mx-auto py-12 px-4">
+      <h2>Breeding Philosophy</h2>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-8 pb-8">
+        <p className="text-lg col-span-2">
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+          commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus
+          et magnis dis parturient montes, nascetur ridiculus mus. Donec quam
+          felis, ultricies nec, pellentesque eu, pretium quis, sem.
+          <br />
+          <br />
+          Nulla consequat massa quis enim. Donec pede justo, fringilla vel,
+          aliquet nec, vulputate eget, arcu. In eni pretium. Integer tincidunt.
+          Cras dapibus. Vivam
+          <br />
+          <br />
+          Nulla consequat massa quis enim. Donec pede justo, fringilla vel,
+          aliquet nec, vulputate eget, arcu. In eni
+        </p>
+        <img
+          src="/images/biz_with_puppies.jpg"
+          className="rounded-sm shadow col-span-1"
+          alt="brown and white dog with two small puppies looks towards the camera surrounded by grass"
+        />
       </div>
+      <Button variant="outline" asChild>
+        <Link href="https://www.facebook.com/puzdog/">LEARN MORE</Link>
+      </Button>
     </section>
   );
 }
--- app/about/components/Origin.tsx
+++ app/about/components/Origin.tsx
@@ -3,33 +3,31 @@ import Link from "next/link";
 
 export default function Origin() {
   return (
-    <section className="bg-primary/10 ">
-      <div className="max-w-[1000px] mx-auto py-12 px-4">
-        <h2>Puzzle Dogs Origin</h2>
-        <div className="flex flex-col gap-8 pb-8">
-          <img src="/images/sara_family.jpg" className="rounded-sm shadow" />
-          <p className="text-lg">
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-            commodo ligula eget dolor. Aenean massa. Cum sociis natoque
-            penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-            Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
-            <br />
-            <br />
-            Nulla consequat massa quis enim. Donec pede justo, fringilla vel,
-            aliquet nec, vulputate eget, arcu. In eni pretium. Integer
-            tincidunt. Cras dapibus. Vivam
-            <br />
-            <br />
-            Nulla consequat massa quis enim. Donec pede justo, fringilla vel,
-            aliquet nec, vulputate eget, arcu. In eni Nulla consequat massa quis
-            enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget,
-            arcu. In eni
-          </p>
-        </div>
-        <Button variant="outline" asChild>
-          <Link href="https://www.facebook.com/puzdog/">LEARN MORE</Link>
-        </Button>
+    <section className="max-w-[1000px] mx-auto py-12 px-4">
+      <h2>Puzzle Dogs Origin</h2>
+      <div className="flex flex-col gap-8 pb-8">
+        <img src="/images/sara_family.jpg" className="rounded-sm shadow" />
+        <p className="text-lg">
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+          commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus
+          et magnis dis parturient montes, nascetur ridiculus mus. Donec quam
+          felis, ultricies nec, pellentesque eu, pretium quis, sem.
+          <br />
+          <br />
+          Nulla consequat massa quis enim. Donec pede justo, fringilla vel,
+          aliquet nec, vulputate eget, arcu. In eni pretium. Integer tincidunt.
+          Cras dapibus. Vivam
+          <br />
+          <br />
+          Nulla consequat massa quis enim. Donec pede justo, fringilla vel,
+          aliquet nec, vulputate eget, arcu. In eni Nulla consequat massa quis
+          enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget,
+          arcu. In eni
+        </p>
       </div>
+      <Button variant="outline" asChild>
+        <Link href="https://www.facebook.com/puzdog/">LEARN MORE</Link>
+      </Button>
     </section>
   );
 }
