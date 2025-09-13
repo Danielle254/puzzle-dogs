ce64dfac2160122f43568b83d3a5e18029a9d84c
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-09-13 16:36:03 -0500

    feat: create section

--- app/about/components/Community.tsx
+++ app/about/components/Community.tsx
@@ -1,3 +1,83 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
+
 export default function Community() {
-  return <div></div>;
+  return (
+    <section className="max-w-[1000px] mx-auto py-12 px-4">
+      <h2>Our Amazing Community</h2>
+      <div className="flex gap-8 pb-8">
+        <Card>
+          <CardHeader>
+            <img
+              src="/images/community.jpg"
+              alt="woman holds dog and ribbon"
+              className="rounded-full"
+            />
+            <CardTitle>
+              <h3 className="text-xl font-semibold text-primary mt-4">
+                Person & Dog!
+              </h3>
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p className="text-lg">
+              fun facts or special accomplishments of this team
+            </p>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader>
+            <img
+              src="/images/Kelli_Riggs.jpg"
+              alt="woman holds dog and ribbon"
+              className="rounded-full"
+            />
+            <CardTitle>
+              <h3 className="text-xl font-semibold text-primary mt-4">
+                Person & Dog!
+              </h3>
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p className="text-lg">
+              fun facts or special accomplishments of this team
+            </p>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader>
+            <img
+              src="/images/Jaceil_Cora.jpg"
+              alt="woman holds dog and ribbon"
+              className="rounded-full"
+            />
+            <CardTitle>
+              <h3 className="text-xl font-semibold text-primary mt-4">
+                Person & Dog!
+              </h3>
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p className="text-lg">
+              fun facts or special accomplishments of this team
+            </p>
+          </CardContent>
+        </Card>
+      </div>
+      <Button asChild>
+        <Link href="https://www.facebook.com/puzdog/">
+          FOLLOW US ON FACEBOOK
+        </Link>
+      </Button>
+    </section>
+  );
 }
