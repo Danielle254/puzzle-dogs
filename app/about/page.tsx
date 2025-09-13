5cf406a6c767eb87654ae7169cff6efbafd33824
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-09-13 15:25:33 -0500

    feat: create about page with sub-components

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,17 @@
+import AboutSara from "./components/AboutSara";
+import BreedingPhilosophy from "./components/BreedingPhilosophy";
+import Community from "./components/Community";
+import Origin from "./components/Origin";
+import WhyMutts from "./components/WhyMutts";
+
 export default function About() {
-  return <div></div>;
+  return (
+    <div>
+      <BreedingPhilosophy />
+      <AboutSara />
+      <Origin />
+      <WhyMutts />
+      <Community />
+    </div>
+  );
 }
