0c1b680ac769fbb33557fe3e96a7e4cc5072cda5
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-09-13 16:13:55 -0500

    style: adjust positioning of h1 element on top of image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,17 +2,18 @@ type HeroProps = {
   image: string;
   alt: string;
   h1: string;
+  imageClasses: string;
 };
 
-export default function Hero({ image, alt, h1 }: HeroProps) {
+export default function Hero({ image, alt, h1, imageClasses }: HeroProps) {
   return (
-    <section className="relative text-center">
+    <section className="relative">
       <img
         src={image}
         alt={alt}
-        className="w-full max-h-[calc(100vh-80px)] object-cover"
+        className={`w-full max-h-[calc(100vh-80px)] object-cover ${imageClasses}`}
       />
-      <div className="absolute text-center top-0 left-0 mt-4 md:mt-12 w-full">
+      <div className="absolute top-0 left-0 text-left lg:text-center mt-4 md:mt-12 w-full lg:w-[1000px] pl-4 lg:pl-0">
         <h1 className="text-2xl sm:text-4xl text-background">{h1}</h1>
       </div>
     </section>
