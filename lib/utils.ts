95a126bf69ac7e1b5ce9e0312c105e1a137bbcc5
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-10-04 11:44:56 -0500

    feat: added filter utility function for our dogs page

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,19 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import type { DogDetailsType, Gender, Status } from "@/app/types/types";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export function filterData(
+  data: DogDetailsType[],
+  filterGender: Gender,
+  filterStatus: Status
+) {
+  return data.filter(
+    (dog) => dog.gender === filterGender && dog.status === filterStatus
+  );
+}
+
+export function calculateAge(dob: string) {}
