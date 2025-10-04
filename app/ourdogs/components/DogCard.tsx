721c15e1fe756648b7727828dd524c92b6db1056
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-10-04 11:58:23 -0500

    feat: begin styling card

--- app/ourdogs/components/DogCard.tsx
+++ app/ourdogs/components/DogCard.tsx
@@ -13,27 +13,38 @@ import {
 import { Badge } from "@/components/ui/badge";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import type { DogDetailsType } from "@/app/types/types";
+import { Carousel } from "@/components/ui/carousel";
 
 export default function DogCard({ dog }: { dog: DogDetailsType }) {
   const [showDetails, setShowDetails] = useState(false);
 
   return (
-    <article>
-      <Card>
-        <CardHeader>
-          <CardTitle>{dog.kennelName}</CardTitle>
-          <CardDescription>
-            {dog.registeredName} {dog.titles}
-          </CardDescription>
-          <Badge>{dog.status}</Badge>
-        </CardHeader>
-        <CardContent>
-          <p>Card Content</p>
-        </CardContent>
-        <CardFooter>
-          <CardAction>See more..</CardAction>
-        </CardFooter>
-      </Card>
-    </article>
+    <Tabs defaultValue="about">
+      <TabsList className="grid w-full lg:w-1/2 grid-cols-2">
+        <TabsTrigger value="about">About</TabsTrigger>
+        <TabsTrigger value="photos">Photos</TabsTrigger>
+      </TabsList>
+      <TabsContent value="about">
+        <Card>
+          <CardHeader>
+            <CardTitle className="text-xl font-bold">{`"${dog.kennelName}"`}</CardTitle>
+            <CardDescription>
+              {dog.registeredName} {dog.titles}
+            </CardDescription>
+            <Badge>{dog.status}</Badge>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+            <CardAction onClick={() => setShowDetails(true)}>
+              See more..
+            </CardAction>
+          </CardContent>
+          <CardFooter></CardFooter>
+        </Card>
+      </TabsContent>
+      <TabsContent value="photos">
+        <Carousel></Carousel>
+      </TabsContent>
+    </Tabs>
   );
 }
