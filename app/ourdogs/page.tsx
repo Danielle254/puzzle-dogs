11dfceb61b2fc19975365e46f76af482a023c7e3
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-10-04 11:57:46 -0500

    feat: create our dogs page with placeholder sections

--- app/ourdogs/page.tsx
+++ app/ourdogs/page.tsx
@@ -1,6 +1,14 @@
 import Hero from "@/components/Hero";
+import CardList from "./components/CardList";
+import { promises as fs } from "fs";
+import path from "path";
+import { filterData } from "@/lib/utils";
+import { DogDetailsType, Gender, Status } from "../types/types";
 
-export default function OurDogs() {
+export default async function OurDogs() {
+  const filePath = path.join(process.cwd(), "app/data/dogData.json");
+  const fileContents = await fs.readFile(filePath, "utf8");
+  const data: DogDetailsType[] = JSON.parse(fileContents);
   return (
     <>
       <Hero
@@ -9,6 +17,18 @@ export default function OurDogs() {
         h1="Our Dogs"
         imageClasses=""
       />
+      <section className="max-w-[1000px] mx-auto py-12 px-4">
+        <h2>Active Girls</h2>
+        <CardList
+          dogs={filterData(data, "female" as Gender, "active" as Status)}
+        />
+      </section>
+      <section className="max-w-[1000px] mx-auto py-12 px-4">
+        <h2>Active Boys</h2>
+        <CardList
+          dogs={filterData(data, "male" as Gender, "active" as Status)}
+        />
+      </section>
     </>
   );
 }
