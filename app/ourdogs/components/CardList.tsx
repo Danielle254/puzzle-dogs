e3cc3dfb626a153f9d36b33efe94f7085a8ae698
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-10-04 11:58:13 -0500

    feat: create cards list by mapping over provided data array

--- app/ourdogs/components/CardList.tsx
+++ app/ourdogs/components/CardList.tsx
@@ -1,5 +1,12 @@
-import React from "react";
+import DogCard from "./DogCard";
+import type { DogDetailsType } from "@/app/types/types";
 
-export default function CardList() {
-  return <div></div>;
+export default function CardList({ dogs }: { dogs: DogDetailsType[] }) {
+  return (
+    <div className="flex flex-col gap-8">
+      {dogs.map((dog) => (
+        <DogCard dog={dog} key={dog.kennelName} />
+      ))}
+    </div>
+  );
 }
