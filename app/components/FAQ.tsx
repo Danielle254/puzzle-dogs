bb6e32b797da3ceb321babb80b2159e530ccd484
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-10-09 16:03:05 -0500

    refactor: modify to use custom accordion component and separate data into variable

--- app/components/FAQ.tsx
+++ app/components/FAQ.tsx
@@ -1,86 +1,46 @@
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
+import CustomAccordion from "@/components/CustomAccordion";
 
 export default function FAQ() {
-  const accordionItemStyling =
-    "bg-background px-2 rounded mb-2 border-primary border-[1px] shadow";
   return (
-    <section className="bg-primary/10">
-      <div className="max-w-[1000px] mx-auto py-12 px-4">
-        <h2>Why Mutts?</h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 pb-8">
-          <Accordion type="single" collapsible className="col-span-2">
-            <AccordionItem value="item-1" className={accordionItemStyling}>
-              <AccordionTrigger className="cursor-pointer">
-                <h3 className="text-xl">Open gene pools</h3>
-              </AccordionTrigger>
-              <AccordionContent>
-                <p className="text-lg">
-                  Closed gene pools shrink generation after generation, and can
-                  result in inbreeding depression and an increased risk of
-                  recessive health disorders over time. Health-tested dogs with
-                  known lineages and performance titles can be found in many
-                  breeds and mixes. What we give up in “type” is made up for in
-                  diversity and a wealth of breeding options!
-                </p>
-              </AccordionContent>
-            </AccordionItem>
-            <AccordionItem value="item-2" className={accordionItemStyling}>
-              <AccordionTrigger className="cursor-pointer">
-                <h3 className="text-xl">Community</h3>
-              </AccordionTrigger>
-              <AccordionContent>
-                <p className="text-lg">
-                  Some of our most beloved pure breeds are facing a need for
-                  outcross programs. We welcome like-minded breeders to
-                  collaborate in order to bring much-needed diversity to pure
-                  breeds who would benefit from outcrossing!
-                </p>
-              </AccordionContent>
-            </AccordionItem>
-            <AccordionItem value="item-3" className={accordionItemStyling}>
-              <AccordionTrigger className="cursor-pointer">
-                <h3 className="text-xl">They’re unique</h3>
-              </AccordionTrigger>
-              <AccordionContent>
-                <p className="text-lg">
-                  One of the coolest things about breeding mutts is seeing the
-                  similarities in our dogs (they’re freeze babies, they’re never
-                  right-side-up, many of them are “smilers,” most of them hug
-                  and like to snuggle under covers…) and also seeing all of the
-                  unique ways that their genetic backgrounds and
-                  socialization/environment combine to make for a truly
-                  one-of-a-kind dog.
-                </p>
-              </AccordionContent>
-            </AccordionItem>
-            <AccordionItem value="item-4" className={accordionItemStyling}>
-              <AccordionTrigger className="cursor-pointer">
-                <h3 className="text-xl">I’m a nerd</h3>
-              </AccordionTrigger>
-              <AccordionContent>
-                <p className="text-lg">
-                  Yes, really! I’m a card-carrying, punnett-square-writing,
-                  Google-scholar-searching dog nerd. As a kid, I memorized the
-                  AKC breeds… all of them. Then I found the UKC. Then the FCI.
-                  So many breeds! As an adult, I think that mixed breeds are the
-                  dog of the future. Genetic diversity, versatility, and a
-                  lessening of selective pressure towards extremes are all
-                  important for the modern dog.
-                </p>
-              </AccordionContent>
-            </AccordionItem>
-          </Accordion>
-          <img
-            src="/images/iroh.jpg"
-            className="rounded-sm shadow col-span-1 max-w-[400px]"
-          />
-        </div>
-      </div>
+    <section className="max-w-[1000px] mx-auto py-12 px-4">
+      <h2>Frequently Asked Questions</h2>
+      <CustomAccordion
+        data={FAQdata}
+        customClasses="[&[data-state='open']]:bg-primary/10"
+      />
     </section>
   );
 }
+
+const FAQdata = [
+  {
+    question: "How much do your puppies cost?",
+    answer:
+      "The dogs themselves are free! You’re paying for my years of pedigree and genetic research, CVT training, puppy developmental knowledge, and the costs (so many costs!) to produce a litter ethically. This oftentimes involves “washing” (not using) multiple dogs before finding a dog whose temperament and health is in line with my breeding goals. The general price for a Puzzle Dog will be in the low-mid four figures. I practice equity, not equality, in puppy pricing, so will place some puppies at a lower cost (eg in junior handler placements) and charge more for others to cover this. I’m happy to set up payment plans.",
+  },
+  {
+    question: "Do you guarantee your dogs’ health and temperament?",
+    answer:
+      "The short answer is no, because genetics are complicated! Breeding dogs isn’t like producing widgets on an assembly line. We can do everything “right,” and you could still end up with a dog who has a health problem (physical or behavioral). Any breeder who claims to have never produced a dog with “issues” is either lying through their teeth, not following up with their puppy buyers, or very new to the game. That said, I do provide a five-year genetic health warranty, and I will always be you and your dog’s biggest advocate and support system if you run into any problems.",
+  },
+  {
+    question: "What do you look for in your puppy homes? ",
+    answer:
+      "Thanks for asking! I look first for people who are obsessed with their dogs. I don’t place my dogs in homes that “need” a dog who will do a specific sport, or who need a specific size of dog (eg a height dog). Instead, I look for placements with people whose dogs are their family members first. I want my dogs to be included in your everyday life! It’s also important to me to produce dogs who are easy to train and live with. As such, I don’t place my dogs in homes that routinely use aversive training methods such as electronic/remote collars, pinches, prongs, chokes, leash corrections, or other aversive methods.",
+  },
+  {
+    question: "What the heck is a partner home? ",
+    answer:
+      "The number of dogs whom I can personally, ethically keep in my own home is low. I’m disabled, and I live in town. In order to ensure that I’m providing exceptional lives to my dog family, I can’t keep a large pack. Partner homes permit me to keep good dogs in our breeding program, while ensuring that every single dog I produce has a family who’s absolutely obsessed with them. Partner homes also provide me with valuable information on the dogs I produce: how they do in a variety of environments, sports, and families! Partner homes are NOT like traditional “guardian” homes. Partners own their dogs outright, but Puzzle Dogs retains breeding rights (with equal and valued input from the partner home about breeding suitability and options).",
+  },
+  {
+    question: "What does your puppy purchasing process look like?",
+    answer:
+      "I place based on a number of factors: suitability for each individual puppy, location (I prefer homes that are closer to me, especially if I don’t already know you!), need (I prefer to place puppies with people who may encounter barriers to getting a dog from a breeder), and more. Once a litter is born, I’ll collect a refundable deposit from a handful of people whom I believe to be the best fits for that litter. Puppy placements are done at or slightly after eight weeks of age, based on structural evaluations completed by our boarded sports medicine and rehabilitation vet, behavioral evaluations, and observations from raising the litter.",
+  },
+  {
+    question: "I want to learn more about breeding. Do you offer mentorships?",
+    answer:
+      "Yes! I’m delighted to help others learn. If you’re local to me, I offer limited mentorships as a “Puppy Nanny” with each litter. You’ll receive lots of hands-on experience and the chance to ask all of your questions about breeding! Not local? I offer very limited, paid consultations at the rate of $200/hour.",
+  },
+];
