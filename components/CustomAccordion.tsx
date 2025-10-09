8ed528d277acaac5e58ee63c4a9c58a9feff860c
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-10-09 16:01:34 -0500

    feat: create reusable custom accordion component

---
+++ components/CustomAccordion.tsx
@@ -0,0 +1,40 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+type AccordionDataType = {
+  question: string;
+  answer: string;
+};
+
+export default function CustomAccordion({
+  data,
+  customClasses,
+}: {
+  data: AccordionDataType[];
+  customClasses: string;
+}) {
+  const accordionItemStyling =
+    "bg-background px-2 rounded mb-2 border-primary border-[1px] shadow";
+  return (
+    <Accordion type="single" collapsible className="col-span-2">
+      {data.map((item) => (
+        <AccordionItem
+          value={item.question}
+          className={`${accordionItemStyling} ${customClasses}`}
+          key={item.question}
+        >
+          <AccordionTrigger className="cursor-pointer">
+            <h3 className="text-xl">{item.question}</h3>
+          </AccordionTrigger>
+          <AccordionContent>
+            <p className="text-lg">{item.answer}</p>
+          </AccordionContent>
+        </AccordionItem>
+      ))}
+    </Accordion>
+  );
+}
