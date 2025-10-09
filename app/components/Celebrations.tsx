97549c4e59345288a05bae7f06749170af490ced
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-10-09 16:02:41 -0500

    feat: create placeholder celebrations component

--- app/components/Celebrations.tsx
+++ app/components/Celebrations.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function Celebrations() {
-  return <div></div>;
+  return (
+    <section className="bg-primary/10">
+      <div className="max-w-[1000px] mx-auto py-12 px-4">
+        <h2>Celebrations</h2>
+      </div>
+    </section>
+  );
 }
