1448d70203cd0e2c7e1fcf81c1c0853cc2cb5f1a
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-10-10 09:59:31 -0500

    fix: rename UpcomingLitters section component to Litters

--- app/components/Litters.tsx
+++ app/components/Litters.tsx
@@ -1,11 +1,11 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
-export default function UpcomingLitters() {
+export default function Litters() {
   return (
     <section className="py-12 px-4 bg-primary/10">
       <div className="max-w-[1000px] mx-auto ">
-        <h2>Upcoming Litters</h2>
+        <h2>Litters</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
           <article className="border border-foreground/20 rounded shadow p-4 bg-background">
             <img
