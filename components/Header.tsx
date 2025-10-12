484e70db550e5350482d756f5196d637e51b3db4
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-10-12 10:24:37 -0500

    feat: add link to home page on header logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,18 @@
 import Nav from "./Nav";
 import MobileNav from "./MobileNav";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <nav className="flex justify-between max-w-[1000px] h-20 mx-auto items-center px-2">
       <div className="flex items-end">
-        <img
-          src="/images/logo/logo_horizontal.png"
-          className="max-h-16"
-          alt="Puzzle Dogs Logo - lime green dog with purple cape leaps up and to the right"
-        />
+        <Link href="/">
+          <img
+            src="/images/logo/logo_horizontal.png"
+            className="max-h-16"
+            alt="Puzzle Dogs Logo - lime green dog with purple cape leaps up and to the right"
+          />
+        </Link>
       </div>
       <MobileNav />
       <Nav classes="hidden md:flex md:gap-4 md:items-center" />
