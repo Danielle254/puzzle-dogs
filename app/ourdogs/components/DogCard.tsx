95e0b6bdc2a17845b87d3c9957b1c5aa8ef0c145
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-11-28 14:48:06 -0600

    fix: remove tabs as photos will now be a button to a 3rd party library

--- app/ourdogs/components/DogCard.tsx
+++ app/ourdogs/components/DogCard.tsx
@@ -8,7 +8,6 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import type { DogDetailsType } from "@/app/types/types";
 import { Carousel } from "@/components/ui/carousel";
 import { calculateAge } from "@/lib/utils";
@@ -32,72 +31,45 @@ export default function DogCard({ dog }: { dog: DogDetailsType }) {
   }
 
   return (
-    <Tabs defaultValue="about">
-      <div className="flex justify-end">
-        <TabsList>
-          <TabsTrigger className="px-4 cursor-pointer" value="about">
-            About
-          </TabsTrigger>
-          <TabsTrigger className="px-4 cursor-pointer" value="photos">
-            Photos
-          </TabsTrigger>
-        </TabsList>
-      </div>
-      <TabsContent value="about">
-        <Card className={dog.status == "Active" ? "border-primary" : ""}>
-          <CardHeader className="flex justify-between">
-            <CardTitle>
-              <h3 className="text-xl font-bold py-1">"{dog.kennelName}"</h3>
-            </CardTitle>
-            <Chip customClasses={modifyChip(dog.status)}>{dog.status}</Chip>
-          </CardHeader>
-          <CardContent className="flex justify-between gap-8">
-            <div className="text-foreground text-lg">
-              <p>
-                {calculateAge(dog.dob)} years old, {dog.breed}
-                <br />
-                {dog.physicalDesc}
-              </p>
+    <>
+      <Card className={dog.status == "Active" ? "border-primary" : ""}>
+        <CardHeader className="flex justify-between">
+          <CardTitle>
+            <h3 className="text-xl font-bold py-1">"{dog.kennelName}"</h3>
+          </CardTitle>
+          <Chip customClasses={modifyChip(dog.status)}>{dog.status}</Chip>
+        </CardHeader>
+        <CardContent className="flex justify-between gap-8">
+          <div className="text-foreground text-lg">
+            <p>
+              {calculateAge(dog.dob)} years old, {dog.breed}
               <br />
-              <p>{dog.owners}</p>
-              <br />
-              <p>
-                Registered Name: {dog.registeredName} {dog.titles}
-              </p>
-              <p>Parents: {dog.parents}</p>
-              <br />
-              <p className="text-base text-foreground mt-4">{dog.about}</p>
-            </div>
-            <img
-              src={dog.profile}
-              alt={dog.physicalDesc}
-              className="h-full max-w-[300px] border-primary border-2 shadow rounded"
-            />
-          </CardContent>
-          <CardFooter className="flex gap-8">
-            {dog.links.map((link) => (
-              <Button
-                variant="link"
-                asChild
-                key={link.path}
-                className="m-0 p-0"
-              >
-                <a href={link.path}>{link.name}</a>
-              </Button>
-            ))}
-          </CardFooter>
-        </Card>
-      </TabsContent>
-      <TabsContent value="photos">
-        <Card>
-          <Carousel></Carousel>
-          <Button asChild variant="link">
-            <Link href="https://www.facebook.com/media/set/?set=a.107599764601568&type=3">
-              Gallery
-            </Link>
-          </Button>
-        </Card>
-      </TabsContent>
-    </Tabs>
+              {dog.physicalDesc}
+            </p>
+            <br />
+            <p>{dog.owners}</p>
+            <br />
+            <p>
+              Registered Name: {dog.registeredName} {dog.titles}
+            </p>
+            <p>Parents: {dog.parents}</p>
+            <br />
+            <p className="text-base text-foreground mt-4">{dog.about}</p>
+          </div>
+          <img
+            src={dog.profile}
+            alt={dog.physicalDesc}
+            className="h-full max-w-[300px] border-primary border-2 shadow rounded"
+          />
+        </CardContent>
+        <CardFooter className="flex gap-8">
+          {dog.links.map((link) => (
+            <Button variant="link" asChild key={link.path} className="m-0 p-0">
+              <a href={link.path}>{link.name}</a>
+            </Button>
+          ))}
+        </CardFooter>
+      </Card>
+    </>
   );
 }
